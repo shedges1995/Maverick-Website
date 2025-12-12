22306ea6c2446b3946a4e432f7544e7c741b1b2f
Sasha Hedges <shedges1995@gmail.com>
2025-12-11 21:34:11 -0600

    latency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ArticlesComponent } from './articles/articles.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
-    },
-    {
-        path: 'articles',
-        component: ArticlesComponent
     }
 ];
